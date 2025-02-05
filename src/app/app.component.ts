82b20f08740021404a7ce819a81fa051e6fdb9b2
HugSTapG <hugo.tapia@uees.edu.ec>
2025-02-05 09:07:24 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '2025_185_UCOM351_02_TALLER_6';
+}
